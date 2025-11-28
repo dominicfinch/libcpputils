803c2b5ec473c95aef2722972d5e5a505b93f257
Dominic Finch <dominic.finch01@gmail.com>
2025-11-28 08:40:49 +0000

    Adding skeleton project

--- manage.sh
+++ manage.sh
@@ -41,9 +41,11 @@ if [ "$1" = "clean" ]; then
 elif [ "$1" == "build-qt" ]; then
     echo " - Building QT components..."
     build_qobject_meta_cpp $script_dir/code/apps/sec-chat/client-ui/include $script_dir/code/apps/sec-chat/client-ui/src
+    build_qobject_meta_cpp $script_dir/code/apps/sec-fdisk/include $script_dir/code/apps/sec-fdisk/src
 elif [ "$1" == "clean-qt" ]; then
     echo " - Cleaning QT generated directories..."
     clean_qobject_meta_cpp $script_dir/code/apps/sec-chat/client-ui/src
+    clean_qobject_meta_cpp $script_dir/code/apps/sec-fdisk/src
 elif [ "$1" == "help" ]; then
     usage
 else
